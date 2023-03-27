e082c657f15f981b1795b9fb58858fe0cebce53a
Rezaan Syed <rezaan.syed@shopify.com>
2023-03-27 17:15:34 -0400

    Fix rename for push

--- packages/app/src/cli/commands/app/push.ts
+++ packages/app/src/cli/commands/app/push.ts
@@ -1,6 +1,6 @@
 import Command from '../../utilities/app-command.js'
 import {appFlags} from '../../flags.js'
-import pushConfig, {PushConfigOptions} from '../../services/app/push-config.js'
+import pushConfig, {PushConfigOptions} from '../../services/app/push.js'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
 
 export default class PushConfig extends Command {
