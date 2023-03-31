c9b41d389e14d5e6dc914b97b5369499e448cb86
Rezaan Syed <rezaan.syed@shopify.com>
2023-03-31 16:35:49 -0400

    create config mapped to a new shopify api client

--- packages/app/src/cli/commands/app/config-create.ts
+++ packages/app/src/cli/commands/app/config-create.ts
@@ -2,6 +2,7 @@ import Command from '../../utilities/app-command.js'
 import {appFlags} from '../../flags.js'
 import configCreate, {ConfigCreateOptions} from '../../services/app/config-create.js'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
+import {Args} from '@oclif/core'
 
 export default class ConfigCreate extends Command {
   static description = 'Create a new configuration.'
@@ -11,11 +12,18 @@ export default class ConfigCreate extends Command {
     ...appFlags,
   }
 
+  static args = {
+    name: Args.string(),
+  }
+
   public async run(): Promise<void> {
-    const {flags} = await this.parse(ConfigCreate)
+    const {flags, args} = await this.parse(ConfigCreate)
+    if (!args.name) throw new Error()
+
     const options: ConfigCreateOptions = {
       commandConfig: this.config,
       directory: flags.path,
+      name: args.name,
     }
     await configCreate(options)
   }
--- packages/app/src/cli/services/app/config-create.ts
+++ packages/app/src/cli/services/app/config-create.ts
@@ -1,34 +1,41 @@
 import {ensureDevContext} from '../context.js'
-import {load, writeConfigurationFile} from '../../models/app/loader.js'
+import {load} from '../../models/app/loader.js'
 import {fetchSpecifications} from '../generate/fetch-extension-specifications.js'
 import {configurationFileNames} from '../../constants.js'
-import {mergeAppConfiguration} from '../merge-configuration.js'
+import {createApp} from '../dev/select-app.js'
+import {fetchOrgFromId} from '../dev/fetch.js'
 import {Config} from '@oclif/core'
 import {ensureAuthenticatedPartners} from '@shopify/cli-kit/node/session'
 import {renderSuccess} from '@shopify/cli-kit/node/ui'
 import {fileExists, writeFileSync} from '@shopify/cli-kit/node/fs'
+import {encodeToml} from '@shopify/cli-kit/node/toml'
 
 export interface ConfigCreateOptions {
   commandConfig: Config
   directory: string
+  name: string
 }
 
 export default async function configCreate(options: ConfigCreateOptions): Promise<void> {
+  const newConfigfile = `${options.directory}/shopify.app.${options.name}.toml`
   const configPath = `${options.directory}/${configurationFileNames.app}`
   const configExists = await fileExists(configPath)
 
   if (!configExists) {
-    writeFileSync(configPath, '')
+    throw new Error("Main App TOML doesn't exist")
   }
+
   const token = await ensureAuthenticatedPartners()
   const {remoteApp} = await ensureDevContext({...options, reset: false}, token, true)
   const apiKey = remoteApp.apiKey
   const specifications = await fetchSpecifications({token, apiKey, config: options.commandConfig})
   const app = await load({directory: options.directory, specifications})
 
-  const mergedLocalApp = mergeAppConfiguration(app, remoteApp)
+  writeFileSync(newConfigfile, encodeToml(app.configuration))
 
-  writeConfigurationFile({...mergedLocalApp})
+  const org = await fetchOrgFromId(remoteApp.organizationId, token)
+
+  await createApp(org, options.name, token)
 
   renderSuccess({
     headline: 'App configuration pulled',
