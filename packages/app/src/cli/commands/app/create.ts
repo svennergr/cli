04d0135d0665af68ecc69e3fd8ecf5c4f693a7cf
Ryan Bahan <ryan.bahan@shopify.com>
2023-04-12 14:11:26 -0600

    wip

---
+++ packages/app/src/cli/commands/app/create.ts
@@ -0,0 +1,56 @@
+import {appFlags} from '../../flags.js'
+import {AppInterface} from '../../models/app/app.js'
+import {load, load as loadApp} from '../../models/app/loader.js'
+import Command from '../../utilities/app-command.js'
+import {loadExtensionsSpecifications} from '../../models/extensions/specifications.js'
+import {Flags} from '@oclif/core'
+import {globalFlags} from '@shopify/cli-kit/node/cli'
+import {outputInfo} from '@shopify/cli-kit/node/output'
+import {renderTextPrompt} from '@shopify/cli-kit/node/ui'
+import {createApp} from '../../services/dev/select-app.js'
+import { fetchOrgFromId } from '../../services/dev/fetch.js'
+import { getAppInfo } from '../../services/local-storage.js'
+import { ensureAuthenticatedPartners } from '@shopify/cli-kit/node/session'
+import { touchFileSync, writeFileSync } from '@shopify/cli-kit/node/fs'
+import { encodeToml } from '@shopify/cli-kit/node/toml'
+
+export default class Create extends Command {
+  static description = 'Create a new app.'
+
+  static flags = {
+    ...globalFlags,
+    ...appFlags,
+    json: Flags.boolean({
+      hidden: false,
+      description: 'format output as JSON',
+      env: 'SHOPIFY_FLAG_JSON',
+    }),
+    'web-env': Flags.boolean({
+      hidden: false,
+      description: 'Outputs environment variables necessary for running and deploying web/.',
+      env: 'SHOPIFY_FLAG_OUTPUT_WEB_ENV',
+      default: false,
+    }),
+  }
+
+  public async run(): Promise<void> {
+    const {flags} = await this.parse(Create)
+    const specifications = await loadExtensionsSpecifications(this.config)
+    const app: AppInterface = await loadApp({specifications, directory: flags.path, mode: 'report'})
+    const appInfo = getAppInfo(flags.path)
+
+    const token = await ensureAuthenticatedPartners()
+    const org = await fetchOrgFromId(appInfo?.orgId!, token);
+
+    const newApp = await createApp(org, "My app", token)
+
+    const newTomlPath = flags.path.concat(`/shopify.app.${app.name}.toml`)
+
+    console.log({newTomlPath})
+    touchFileSync(newTomlPath)
+
+    writeFileSync(newTomlPath, encodeToml(app.configuration));
+
+    if (app.errors) process.exit(2)
+  }
+}
