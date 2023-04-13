338c3b80f92bab3d3f85f333fe6ce0f26456b46d
Rezaan Syed <rezaan.syed@shopify.com>
2023-04-13 15:13:05 -0400

    rename pull to sync

--- packages/app/src/cli/commands/app/sync.ts
+++ packages/app/src/cli/commands/app/sync.ts
@@ -4,7 +4,7 @@ import pullConfig, {PullConfigOptions} from '../../services/app/pull.js'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
 import {Args} from '@oclif/core'
 
-export default class PullConfig extends Command {
+export default class SyncConfig extends Command {
   static description = 'Pull your app configuration.'
 
   static flags = {
@@ -17,7 +17,7 @@ export default class PullConfig extends Command {
   }
 
   public async run(): Promise<void> {
-    const {flags, args} = await this.parse(PullConfig)
+    const {flags, args} = await this.parse(SyncConfig)
     const options: PullConfigOptions = {
       commandConfig: this.config,
       directory: flags.path,
