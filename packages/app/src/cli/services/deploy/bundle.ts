9d38a1e3c5d590bea9a0a5274c54c4e41dd5b85b
Ryan Ische <ryan.ische@shopify.com>
2023-05-24 17:47:33 +0000

    Exploring new Flow action extension type

--- packages/app/src/cli/services/deploy/bundle.ts
+++ packages/app/src/cli/services/deploy/bundle.ts
@@ -48,7 +48,9 @@ export async function bundleAndBuildExtensions(options: BundleOptions) {
               ui: options.app.extensions.ui
                 .filter((uiExtension) => {
                   // excluding flow action definition extensions from being built.
-                  return uiExtension.type !== 'flow_action_definition' && uiExtension.type !== 'flow_trigger_definition'
+                  return (
+                    uiExtension.type !== 'flow_action_definition_2' && uiExtension.type !== 'flow_trigger_definition'
+                  )
                 })
                 .map((uiExtension) => {
                   const extensionId = options.identifiers.extensions[uiExtension.localIdentifier]!
