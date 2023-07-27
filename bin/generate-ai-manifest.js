229e49a1143214244b0afeed5c222ffb4c10f24a
Matteo Depalo <matteo.depalo@shopify.com>
2023-07-27 10:15:43 +0100

    Update magic script to generate embeddings

--- bin/generate-ai-manifest.js
+++ bin/generate-ai-manifest.js
@@ -34,42 +34,6 @@ const concatenateAndCleanManifests = (files) => {
   }, {});
 };
 
-// Function to fetch the content of a webpage
-const fetchWebpageContent = (url) => {
-  return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        const mainContentMatch = data.match(/<main[^>]*>([\s\S]*?)<\/main>/i);
-        const mainContent = mainContentMatch ? mainContentMatch[1] : '';
-
-        // Remove HTML tags, JavaScript code, and extra whitespace
-        const textContent = mainContent
-          .replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
-          .replace(/<[^>]*>/g, '')
-          .replace(/[\t\r ]+\n/g, '\n') // remove spaces/tabs preceding line breaks
-          .replace(/\n[\t\r ]+/g, '\n') // remove spaces/tabs following line breaks
-          .replace(/\n+/g, ' ') // replace line breaks with a space
-          .trim();
-        resolve(textContent);
-      });
-    }).on('error', (err) => {
-      reject(err);
-    });
-  });
-};
-
-const fetchDocs = async (urls) => {
-  const docs = {};
-  for (const url of urls) {
-    docs[url] = await fetchWebpageContent(url);
-  }
-  return docs;
-};
-
 const files = [
   'packages/app/oclif.manifest.json',
   'packages/cli/oclif.manifest.json',
@@ -78,24 +42,5 @@ const files = [
 
 const concatenatedAndCleanedManifests = concatenateAndCleanManifests(files);
 
-const docsUrls = [
-  'https://shopify.dev/docs/apps/tools/cli',
-  'https://shopify.dev/docs/apps/tools/cli/commands',
-  'https://shopify.dev/docs/apps/tools/cli/structure',
-  'https://shopify.dev/docs/apps/tools/cli/existing',
-  'https://shopify.dev/docs/apps/app-extensions/list',
-  'https://shopify.dev/docs/apps/marketing/pixels'
-];
-
-fetchDocs(docsUrls).then((docs) => {
-  const output = {
-    commands: concatenatedAndCleanedManifests,
-    docs,
-  };
-
-  fs.writeFileSync('ai.json', JSON.stringify(output));
-
-  console.log('Done!');
-}).catch((error) => {
-  console.error(`Error fetching docs: ${error}`);
-});
+fs.writeFileSync('ai.json', JSON.stringify(concatenatedAndCleanedManifests));
+console.log('Done!');
--- packages/app/src/cli/services/magic.ts
+++ packages/app/src/cli/services/magic.ts
@@ -1,10 +1,15 @@
 import {renderInfo, renderTasks, renderTextPrompt} from '@shopify/cli-kit/node/ui'
 import {ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate} from 'langchain/prompts'
 import {resolvePath, joinPath, dirname} from '@shopify/cli-kit/node/path'
-import {readFile} from '@shopify/cli-kit/node/fs'
-import {ChatOpenAI} from 'langchain/chat_models/openai'
 import {z} from 'zod'
+import {HNSWLib} from 'langchain/vectorstores/hnswlib'
+import {OpenAIEmbeddings} from 'langchain/embeddings/openai'
 import {createStructuredOutputChainFromZod} from 'langchain/chains/openai_functions'
+import {ChatOpenAI} from 'langchain/chat_models/openai'
+import {fileExistsSync, readFile} from '@shopify/cli-kit/node/fs'
+import {RecursiveCharacterTextSplitter} from 'langchain/text_splitter'
+import {StuffDocumentsChain} from 'langchain/chains'
+import {fetch} from '@shopify/cli-kit/node/http'
 import {fileURLToPath} from 'url'
 
 const zodSchema = z.object({
@@ -21,10 +26,17 @@ export async function magic({query}: {query?: string}) {
     })
   }
 
-  const template = 'You are an assistant to a Shopify partner who is building an app with the Shopify CLI.'
-  const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(template)
-  const humanTemplate = `In the JSON below, you'll find two keys: "commands" and "docs". The "commands" key contains the oclif manifests the Shopify CLI, while the "docs" key contains the documentation of the Shopify CLI.
+  const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(
+    'You are an assistant to a Shopify partner who is building an app with the Shopify CLI.',
+  )
+  const humanTemplate = `In the JSON below, delimited by \`\`\`json. you'll find the oclif manifests the Shopify CLI.
+  Commands are in the form of \`app:build\`, which translate to \`shopify app build\` in the terminal.
   \`\`\`json
+  {oclifManifests}
+  \`\`\`
+
+  The following, delimited by \`\`\` is a list of all documentation pages relevant to the user query:
+  \`\`\`
   {context}
   \`\`\`
 
@@ -36,34 +48,89 @@ export async function magic({query}: {query?: string}) {
   const __dirname = dirname(__filename)
   const rootDir = resolvePath(__dirname, '../../../../..')
   const fileToRead = joinPath(rootDir, 'ai.json')
-  const aiContext = await readFile(fileToRead)
+  const oclifManifests = await readFile(fileToRead)
+  const embeddingsDir = joinPath(rootDir, 'embeddings')
+
+  // Create a vector store from the documents.
+  let vectorStore: HNSWLib
+
+  if (fileExistsSync(embeddingsDir)) {
+    vectorStore = await HNSWLib.load(embeddingsDir, new OpenAIEmbeddings())
+  } else {
+    await renderTasks([
+      {
+        title: 'Creating embeddings',
+        task: async () => {
+          const docsUrls = [
+            'https://shopify.dev/docs/apps/tools/cli',
+            'https://shopify.dev/docs/apps/tools/cli/commands',
+            'https://shopify.dev/docs/apps/tools/cli/structure',
+            'https://shopify.dev/docs/apps/tools/cli/existing',
+            'https://shopify.dev/docs/apps/app-extensions/list',
+            'https://shopify.dev/docs/apps/marketing/pixels',
+          ]
+
+          const splitter = RecursiveCharacterTextSplitter.fromLanguage('html', {
+            chunkSize: 2000,
+            chunkOverlap: 200,
+          })
+
+          const texts = []
+          for (const url of docsUrls) {
+            // eslint-disable-next-line no-await-in-loop
+            const response = await fetch(url)
+            // eslint-disable-next-line no-await-in-loop
+            const text = await response.text()
+            // keep only the <main> tag from the text
+            const mainTag = text.match(/<main.*?>([\s\S]*?)<\/main>/)
+            texts.push(mainTag![1]!)
+          }
+
+          const docs = await splitter.createDocuments(texts)
+          vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings())
+          await vectorStore.save(embeddingsDir)
+        },
+      },
+    ])
+  }
+
+  // Initialize a retriever wrapper around the vector store
+  const vectorStoreRetriever = vectorStore!.asRetriever()
+  const relevantDocs = await vectorStoreRetriever.getRelevantDocuments(userPrompt)
+
+  // eslint-disable-next-line no-console
+  console.log(relevantDocs)
 
   const llm = new ChatOpenAI({
     temperature: 0,
     modelName: 'gpt-3.5-turbo-16k-0613',
   })
 
-  const chain = createStructuredOutputChainFromZod(zodSchema, {
+  const llmChain = createStructuredOutputChainFromZod(zodSchema, {
     llm,
     prompt,
   })
 
-  let response
+  const chain = new StuffDocumentsChain({llmChain})
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let response: {[key: string]: any}
 
   await renderTasks([
     {
       title: 'Thinking',
       task: async () => {
         response = await chain.call({
-          context: aiContext,
+          input_documents: relevantDocs,
           userPrompt,
+          oclifManifests,
+          verbose: true,
         })
       },
     },
   ])
 
   renderInfo({
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     headline: ['Run', {command: response!.output.command}, {char: '.'}],
   })
 }
